d50e7c68a586c567a8bd012280f9cb0de686055b
moonyu <dbwls3681@naver.com>
2024-08-28 17:31:52 +0900

    chapter_04 과제 업로드

--- chapter_3/config/settings.py
+++ chapter_3/config/settings.py
@@ -125,5 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # login / logout
-LOGIN_REDIRECT_URL = '/todo/' # 로그인 후에 To Do List 페이지로 이동하기 위함
-LOGOUT_REDIRECT_URL = '/accounts/login/' # 로그아웃 시 로그인 페이지로 이동하기 위함
+# login / logout
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/todo/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
